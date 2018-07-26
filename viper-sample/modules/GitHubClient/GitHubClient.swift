b293df654c9569edf1b0cb5f5b17d9d9536ee1a8
hicka04 <calpissoda8221@gmail.com>
2018-07-26 16:40:47 +0900

    GitHubにリクエストを送るクライアント作成

---
+++ viper-sample/modules/GitHubClient/GitHubClient.swift
@@ -0,0 +1,45 @@
+//
+//  GitHubClient.swift
+//  viper-sample
+//
+//  Created by hicka04 on 2018/07/26.
+//  Copyright © 2018年 hicka04. All rights reserved.
+//
+
+import Foundation
+
+enum Result<T> {
+    case success(T)
+    case failure
+}
+
+class GitHubClient {
+
+    private let session: URLSession = {
+        let configuration = URLSessionConfiguration.default
+        let session = URLSession(configuration: configuration)
+        return session
+    }()
+    
+    func send<Request: GitHubRequest>(request: Request, completion: @escaping (Result<Request.Response>) -> ()) {
+        let urlRequest = request.buildURLRequest()
+        let task = session.dataTask(with: urlRequest) { (data, response, error) in
+            switch (data, response, error) {
+            case (_, _, let error?):
+                print(error.localizedDescription)
+                completion(.failure)
+            case (let data?, let response?, _):
+                do {
+                    let response = try request.response(from: data, urlResponse: response)
+                    completion(.success(response))
+                } catch {
+                    completion(.failure)
+                }
+            default:
+                completion(.failure)
+            }
+        }
+        
+        task.resume()
+    }
+}
